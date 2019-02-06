8caf5082d618c90b2b22f590fe2bb7d845d3f85f
Stanislav Kravchenko <stankra@yandex.ru>
2019-02-06 12:51:22 +0300

    [elixir][2] Speaking with Bob (cond, regex)

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,24 @@
 defmodule Bob do
-  @type input() :: String.t()
-
-  @spec hey(input()) :: String.t()
+  @spec hey(String.t()) :: String.t()
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      yell?(input) && question?(input) -> "Calm down, I know what I'm doing!"
+      yell?(input)                     -> "Whoa, chill out!"
+      question?(input)                 -> "Sure."
+      empty?(input)                    -> "Fine. Be that way!"
+      true                             -> "Whatever."
     end
+  end
+
+  defp yell?(input) do
+    (input =~ ~r/\p{Lu}/u) && !(input =~ ~r/\p{Ll}/u)
+  end
+
+  defp question?(input) do
+    input =~ ~r/\?$/
+  end
+
+  defp empty?(input) do
+    input =~ ~r/^\s*$/
   end
 end
