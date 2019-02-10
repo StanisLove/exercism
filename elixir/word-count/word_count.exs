c149bf2febb86d4fa39e86174ebec6bf74ec6a99
Stanislav Kravchenko <stankra@yandex.ru>
2019-02-10 23:57:11 +0300

    [erlang][3] follow review

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,19 +6,31 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    regex = ~r/(?<=\w)\p{P}(?!\w)|(?<!\w)\p{P}(?=\w)|(?<!\w)\p{P}(?!\w)|\p{S}|_/u
+    String.split(sentence, regex(), trim: true)
+    |> List.foldl(%{}, &count/2)
+  end
 
-    String.replace(sentence, regex, " ")
-    |> String.split
-    |> List.foldl(%{}, fn word, accum -> count(word, accum) end)
+  defp regex do
+    regexps = [
+      # punctuation at the end of a word
+      "(?<=\\w)\\p{P}(?!\\w)",
+      # punctuation at the beginning of a word
+      "(?<!\\w)\\p{P}(?=\\w)",
+      # punctuation between not a letter chars
+      "(?<!\\w)\\p{P}(?!\\w)",
+      # symbols
+      "\\p{S}",
+      # underscores
+      "_",
+      # spaces
+      "\\s"
+    ]
+
+    Enum.join(regexps, "|") |> Regex.compile!([:unicode])
   end
 
   defp count(word, accum) do
     key = String.downcase(word)
-    value = accum[key]
-    count(key, value, accum)
+    Map.update(accum, key, 1, &(&1 + 1))
   end
-
-  defp count(key, nil, accum), do: Map.put(accum, key, 1)
-  defp count(key, value, accum), do: Map.put(accum, key, value + 1)
 end
