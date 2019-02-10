60a5d173d7935246e3a0be37253583f89f82334d
Stanislav Kravchenko <stankra@yandex.ru>
2019-02-10 23:57:11 +0300

    [erlang][3] follow review

--- erlang/leap/src/leap.erl
+++ erlang/leap/src/leap.erl
@@ -2,10 +2,7 @@
 
 -export([leap_year/1]).
 
-leap_year(Year) when is_integer(Year) ->
-  if
-    (Year rem 4 =:= 0) and (Year rem 100 =/= 0) -> true;
-    (Year rem 4 =:= 0) and (Year rem 100 =:= 0) -> Year rem 400 =:= 0;
-    true -> false
-  end;
-leap_year(_) -> "Year must be integer".
+leap_year(Year) when Year rem 400 =:= 0 -> true;
+leap_year(Year) when Year rem 100 =:= 0 -> false;
+leap_year(Year) when Year rem 4 =:= 0 -> true;
+leap_year(_Year) -> false.
