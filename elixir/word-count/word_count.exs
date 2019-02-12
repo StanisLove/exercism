a197b3497d958ca4ffada00d8751be934b5b8582
Stanislav Kravchenko <stankra@yandex.ru>
2019-02-12 16:21:59 +0300

    [elixir][3] Word count: follow review

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,27 +6,8 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    String.split(sentence, regex(), trim: true)
+    String.split(sentence, ~r/[^[:alnum:]-]/u, trim: true)
     |> List.foldl(%{}, &count/2)
-  end
-
-  defp regex do
-    regexps = [
-      # punctuation at the end of a word
-      "(?<=\\w)\\p{P}(?!\\w)",
-      # punctuation at the beginning of a word
-      "(?<!\\w)\\p{P}(?=\\w)",
-      # punctuation between not a letter chars
-      "(?<!\\w)\\p{P}(?!\\w)",
-      # symbols
-      "\\p{S}",
-      # underscores
-      "_",
-      # spaces
-      "\\s"
-    ]
-
-    Enum.join(regexps, "|") |> Regex.compile!([:unicode])
   end
 
   defp count(word, accum) do
