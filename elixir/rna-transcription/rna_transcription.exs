02bbcc2f524330a321fe11bc04918a6b42feaaf3
Stanislav Kravchenko <stankra@yandex.ru>
2019-02-15 23:25:54 +0300

    [elixir][4] follow review

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -8,9 +8,10 @@ defmodule RNATranscription do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna('G'), do: 'C'
-  def to_rna('C'), do: 'G'
-  def to_rna('T'), do: 'A'
-  def to_rna('A'), do: 'U'
-  def to_rna(dna), do: Enum.map(dna, &to_rna/1)
+  def to_rna(dna), do: Enum.map(dna, &char_to_rna/1)
+
+  defp char_to_rna(?G), do: ?C
+  defp char_to_rna(?C), do: ?G
+  defp char_to_rna(?T), do: ?A
+  defp char_to_rna(?A), do: ?U
 end
