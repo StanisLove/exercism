a9595bdcc8107d9da0be0604a6b12db48133ac96
Stanislav Kravchenko <stankra@yandex.ru>
2019-03-13 12:30:44 +0300

    [erlang][6] follow review

--- erlang/rna-transcription/src/rna_transcription.erl
+++ erlang/rna-transcription/src/rna_transcription.erl
@@ -4,9 +4,9 @@
 
 
 to_rna(Strand) ->
-  [char_to_rna([Char]) || Char <- Strand].
+  [char_to_rna(Char) || Char <- Strand].
 
-char_to_rna("G") -> [Char] = "C", Char;
-char_to_rna("C") -> [Char] = "G", Char;
-char_to_rna("T") -> [Char] = "A", Char;
-char_to_rna("A") -> [Char] = "U", Char.
+char_to_rna($G) -> $C;
+char_to_rna($C) -> $G;
+char_to_rna($T) -> $A;
+char_to_rna($A) -> $U.
