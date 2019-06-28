70a9b39cac5d7f1e42ff0d9778d48340ba1c05e7
Stanislav Kravchenko <stankra@yandex.ru>
2019-06-28 08:10:34 +0300

    [bash][3] follow review

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 main() {
-  if [[ $# == 1 ]]
-  then
-    echo "Hello, $1"
-  else
+  if (( $# != 1 )); then
     echo >&2 "Usage: ./error_handling <greetee>"
     exit 1
   fi
+
+  echo "Hello, $1"
 }
 
 main "$@"
