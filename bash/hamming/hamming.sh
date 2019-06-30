f3d5ac43dd6cb6683ab95d10c7b282dfae634832
Stanislav Kravchenko <stankra@yandex.ru>
2019-06-30 13:46:06 +0300

    [bash][5] DNA (for loop, declare)

---
+++ bash/hamming/hamming.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+main() {
+  (( $# != 2 )) && raise 'Usage: hamming.sh <string1> <string2>'
+  [[ ${#1} -ne ${#2} ]] && raise 'left and right strands must be of equal length'
+
+  declare -i result=0
+
+  for ((i = 0; i < ${#1}; i++)); do
+    [[ ${1:$i:1} != ${2:$i:1} ]] && result=$((result + 1))
+  done
+
+  echo $result
+}
+
+raise() {
+  echo >&2 $1
+  exit 1
+}
+
+main "$@"
